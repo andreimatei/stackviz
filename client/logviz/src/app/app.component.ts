b9ab4fec7493c914909914b2f76512afa533750e
Andrei Matei <andrei@cockroachlabs.com>
2023-05-08 10:30:01 -0400

    add logviz

---
+++ client/logviz/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AppCoreService } from 'traceviz/dist/ngx-traceviz-lib';
+
+/** The application component of the LogViz client. */
+@Component({
+  selector: 'logviz',
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css'],
+})
+export class AppComponent {
+  constructor(public appCoreService: AppCoreService) {
+  }
+}
---
+++ client/logviz/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {CoreModule} from 'traceviz/dist/ngx-traceviz-lib';
+import { DataTableModule } from 'traceviz/dist/ngx-traceviz-lib';
+import { WeightedTreeModule } from 'traceviz/dist/ngx-traceviz-lib';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    DataTableModule,
+    HttpClientModule,
+    MatCardModule,
+    MatPaginatorModule,
+    WeightedTreeModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
