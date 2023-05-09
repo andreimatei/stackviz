28f070c02e8e74b383bccff39a5a55007f612dce
Andrei Matei <andreimatei1@gmail.com>
2023-05-09 15:29:36 -0400

    frontend cleanup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,19 +13,6 @@ export class AppComponent {
   constructor(public readonly appCoreService: AppCoreService) {
   }
 
-
   ngAfterContentInit(): void {
-    console.log('AppComponent: ngAfterContentInit');
-    this.appCoreService.appCore.onPublish((appCore) => {
-      console.log('AppComponent: onPublish callback');
-      appCore.globalState.subscribe(v => {
-        console.log("global state update: ", v)
-        if (v.indexOf("dummy") != -1) {
-          appCore.globalState.get("dummy").subscribe((val: any) => {
-            console.log("updated value - ", (val as StringValue).val);
-          })
-        }
-      })
-    })
   }
 }
