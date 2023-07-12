bfdfbd012f53ab4eb98fcab3ebf19f90aa5cd910
Andrei Matei <andreimatei1@gmail.com>
2023-07-12 16:37:33 -0400

    cleanup

--- client/src/app/components/backtrace/backtrace.component.ts
+++ client/src/app/components/backtrace/backtrace.component.ts
@@ -9,55 +9,55 @@ import { RouterLink } from "@angular/router";
   standalone: true,
   imports: [CommonModule, MatTableModule, RouterLink],
   template: `
-      <table mat-table [dataSource]="vars" *ngIf="vars && vars.length > 0">
-          <ng-container matColumnDef="expr">
-              <th mat-header-cell *matHeaderCellDef>Expression</th>
-              <td mat-cell *matCellDef="let v">
-                  {{v.Expr}}
-              </td>
-          </ng-container>
-          <ng-container matColumnDef="val">
-              <th mat-header-cell *matHeaderCellDef>Value</th>
-              <td mat-cell *matCellDef="let v">
-                  {{v.Value}}
-              </td>
-          </ng-container>
-          <ng-container matColumnDef="links">
-              <th mat-header-cell *matHeaderCellDef>Links</th>
-              <td mat-cell *matCellDef="let v">
-                  <div *ngIf="v.Links?.length > 0">
-                      <ul>
-                          <li *ngFor="let l of v.Links">
-                            <a
-                              [routerLink]="['/collections', collectionID, 'snap',l.SnapshotID]"
-                              [queryParams]="{filter: 'gid=' + l.GoroutineID}"
-                            >
-                              Snapshot: {{l.SnapshotID}} Goroutine: {{l.GoroutineID}}
-                            </a>
-                          </li>
-                      </ul>
-                  </div>
-              </td>
-          </ng-container>
-          <tr mat-header-row *matHeaderRowDef="['expr', 'val', 'links']"></tr>
-          <tr mat-row *matRowDef="let rowData; columns: ['expr','val', 'links']"></tr>
-      </table>
-      <table mat-table [dataSource]="frames">
-          <ng-container matColumnDef="index">
-              <th mat-header-cell *matHeaderCellDef></th>
-              <td mat-cell class="cell-index" *matCellDef="let rIndex = index;"> {{rIndex}}</td>
-          </ng-container>
-          <ng-container matColumnDef="func">
-              <th mat-header-cell *matHeaderCellDef>Backtrace</th>
-              <td mat-cell *matCellDef="let frame">{{frame.Func}}</td>
-          </ng-container>
-          <ng-container matColumnDef="location">
-              <th mat-header-cell *matHeaderCellDef>file:line</th>
-              <td mat-cell *matCellDef="let frame">{{frame.File}}:{{frame.Line}}</td>
-          </ng-container>
-          <tr mat-header-row *matHeaderRowDef="['index','func','location']"></tr>
-          <tr mat-row *matRowDef="let rowData; columns: ['index','func','location']"></tr>
-      </table>
+    <table mat-table [dataSource]="vars" *ngIf="vars && vars.length > 0">
+      <ng-container matColumnDef="expr">
+        <th mat-header-cell *matHeaderCellDef>Expression</th>
+        <td mat-cell *matCellDef="let v">
+          {{v.Expr}}
+        </td>
+      </ng-container>
+      <ng-container matColumnDef="val">
+        <th mat-header-cell *matHeaderCellDef>Value</th>
+        <td mat-cell *matCellDef="let v">
+          {{v.Value}}
+        </td>
+      </ng-container>
+      <ng-container matColumnDef="links">
+        <th mat-header-cell *matHeaderCellDef>Links</th>
+        <td mat-cell *matCellDef="let v">
+          <div *ngIf="v.Links?.length > 0">
+            <ul>
+              <li *ngFor="let l of v.Links">
+                <a
+                    [routerLink]="['/collections', collectionID, 'snap',l.SnapshotID]"
+                    [queryParams]="{filter: 'gid=' + l.GoroutineID}"
+                >
+                  Snapshot: {{l.SnapshotID}} Goroutine: {{l.GoroutineID}}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </td>
+      </ng-container>
+      <tr mat-header-row *matHeaderRowDef="['expr', 'val', 'links']"></tr>
+      <tr mat-row *matRowDef="let rowData; columns: ['expr','val', 'links']"></tr>
+    </table>
+    <table mat-table [dataSource]="frames">
+      <ng-container matColumnDef="index">
+        <th mat-header-cell *matHeaderCellDef></th>
+        <td mat-cell class="cell-index" *matCellDef="let rIndex = index;"> {{rIndex}}</td>
+      </ng-container>
+      <ng-container matColumnDef="func">
+        <th mat-header-cell *matHeaderCellDef>Backtrace</th>
+        <td mat-cell *matCellDef="let frame">{{frame.Func}}</td>
+      </ng-container>
+      <ng-container matColumnDef="location">
+        <th mat-header-cell *matHeaderCellDef>file:line</th>
+        <td mat-cell *matCellDef="let frame">{{frame.File}}:{{frame.Line}}</td>
+      </ng-container>
+      <tr mat-header-row *matHeaderRowDef="['index','func','location']"></tr>
+      <tr mat-row *matRowDef="let rowData; columns: ['index','func','location']"></tr>
+    </table>
   `,
   styleUrls: ['./backtrace.component.css']
 })
